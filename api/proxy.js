3c6b4a6860e61451e4209a2343319d828655655e
YumeTravelAssistant <progettogiappone@gmail.com>
2025-04-19 18:03:57 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://melodious-malabi-90a97a.netlify.app");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+ res.setHeader("Access-Control-Allow-Origin", "https://melodious-malabi-90a97a.netlify.app");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
